b296f763170d7e485bbe69ef01bff51b22a43653
phillipcrane <125037167+phillipcrane@users.noreply.github.com>
2026-03-20 21:08:58 +0100

    change ports

--- src/test/java/dk/unievent/web/media/MediaControllerTests.java
+++ src/test/java/dk/unievent/web/media/MediaControllerTests.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.http.MediaType;
