17d19e2bfa911b53f5582b452a773dfd4bb39450
phillipcrane <125037167+phillipcrane@users.noreply.github.com>
2026-05-05 17:30:07 +0200

    update .gitignore

--- src/test/java/dk/unievent/app/tools/controller/TokenControllerTests.java
+++ src/test/java/dk/unievent/app/tools/controller/TokenControllerTests.java
@@ -19,7 +19,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
-class TokenRefreshControllerTests {
+class TokenControllerTests {
 
     @Mock
     private TokenRefreshService tokenRefreshService;
